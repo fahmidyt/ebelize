6a7fbccbb9a6c2a3229b9f7ad392ab545086aa62
fahmidyt <fmidyt@gmail.com>
2024-07-08 00:03:32 +0700

    refactor: rateLimit plugin to include IP address validation

--- src/core/plugins/rateLimit.ts
+++ src/core/plugins/rateLimit.ts
@@ -6,6 +6,17 @@ const rateLimitOptions: Partial<Options> = {
   message: "Too many requests, please try again later.", // message to send
   legacyHeaders: false, // disable X-RateLimit-Limit and X-RateLimit-Remaining headers
   standardHeaders: "draft-7", // enable standard rate limit headers
+  keyGenerator: (req) => {
+    if (req.user) return req.user.id; // generate a key for each request based on user ID (if authenticated
+
+    if (req.clientIp) return req.clientIp; // generate a key for each request based on IP address
+
+    // fallback to path and useragent
+    return `${req.path}-${req.useragent?.browser ?? "unknown"}`;
+  }, // generate a key for each request based on IP address
+  validate: {
+    ip: true, // enable IP address validation
+  },
 };
 
 export default rateLimit(rateLimitOptions);
